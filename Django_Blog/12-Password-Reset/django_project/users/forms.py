92de1ac46bb428768960887cfdfaec010a5ead78
John Fake <jwfakejr@gmail.com>
2018-09-13 13:46:23 -0500

    Chgs for checking for duplicate email

--- Django_Blog/12-Password-Reset/django_project/users/forms.py
+++ Django_Blog/12-Password-Reset/django_project/users/forms.py
@@ -11,6 +11,20 @@ class UserRegisterForm(UserCreationForm):
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
+    def clean_email(self):
+        users = User.objects.all().values()
+        dup_email = False
+        for user in users:
+            print(user)
+            if 'email' in user:
+
+                if user['email'] == self.cleaned_data.get('email'):
+                    print('found duplicate email')
+                    dup_email = True
+        if dup_email is True:
+            raise forms.ValidationError('email is already being used!')
+        else:
+            return self.cleaned_data.get('email')
 
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
--- Django_Blog/12-Password-Reset/django_project/users/views.py
+++ Django_Blog/12-Password-Reset/django_project/users/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
-
+from django import forms
+from django.contrib.auth.models import User
 
 def register(request):
     if request.method == 'POST':
@@ -17,9 +18,11 @@ def register(request):
     return render(request, 'users/register.html', {'form': form})
 
 
+
 @login_required
 def profile(request):
     if request.method == 'POST':
+
         u_form = UserUpdateForm(request.POST, instance=request.user)
         p_form = ProfileUpdateForm(request.POST,
                                    request.FILES,
@@ -29,7 +32,6 @@ def profile(request):
             p_form.save()
             messages.success(request, f'Your account has been updated!')
             return redirect('profile')
-
     else:
         u_form = UserUpdateForm(instance=request.user)
         p_form = ProfileUpdateForm(instance=request.user.profile)
